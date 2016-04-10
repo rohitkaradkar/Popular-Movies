360d6742394dfe9f23088456a75c1abe334e8f50
Rohit Karadkar <karadkar.rohit@gmail.com>
2016-04-10 10:19:37 +0530

    Creating Two pane layout

--- app/src/main/java/com/example/rnztx/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/rnztx/popularmovies/MainActivity.java
@@ -5,16 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     final String LOG_TAG = MainActivity.class.getSimpleName();
-
+    private static boolean mTwoPane = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.mainActivity_container,new MainFragment())
-                    .commit();
+
+        if(findViewById(R.id.MovieDetail_container)!=null){
+            mTwoPane = true;
+            if(savedInstanceState == null){
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.mainActivity_container,new MainFragment())
+                        .commit();
+            }
+
+        }else {
+            mTwoPane = false;
         }
     }
 }
